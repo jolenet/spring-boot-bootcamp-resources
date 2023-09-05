9dd8b43c185eb9b731144109cbfa071114936b7d
Jolene <mjolenetay@gmail.com>
2023-09-05 11:03:16 +0800

    rest of section9

---
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/Constants.java
@@ -0,0 +1,4 @@
+package com.ltp.gradesubmission;
+public class Constants {
+    public static final int NOT_FOUND = -1000;
+}
--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -1,9 +1,12 @@
 package com.ltp.gradesubmission;
 
+import java.util.UUID;
+
 public class Grade {
 	private String name;
 	private String subject;
 	private String score;
+	private String id;
 
 
 	public Grade(String name, String subject, String score) {
@@ -13,6 +16,7 @@ public class Grade {
 	}
 
 	public Grade() {
+		this.id = UUID.randomUUID().toString();
 	}
 
 	public String getName() {
@@ -37,6 +41,23 @@ public class Grade {
 
 	public void setScore(String score) {
 		this.score = score;
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+			" name='" + getName() + "'" +
+			", subject='" + getSubject() + "'" +
+			", score='" + getScore() + "'" +
+			"}";
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }
