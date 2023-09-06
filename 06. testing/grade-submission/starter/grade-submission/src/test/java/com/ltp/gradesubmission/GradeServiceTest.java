d4b358faee4c5a137748bc9077a066d3bd73f536
Jolene <mjolenetay@gmail.com>
2023-09-06 10:08:11 +0800

    145

--- 06. testing/grade-submission/starter/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
+++ 06. testing/grade-submission/starter/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -1,6 +1,8 @@
 package com.ltp.gradesubmission;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -29,12 +31,66 @@ public class GradeServiceTest {
     public void getGradesFromRepoTest(){
         when(gradeRepository.getGrades()).thenReturn(Arrays.asList(
             new Grade("Harry", "Potions", "C-"),
-            new Grade("Hermoine", "Arithmancy", "a+")
+            new Grade("Hermoine", "Arithmancy", "A+")
         ));
 
         //store result in arraylist
         List<Grade> result = gradeService.getGrades();
 
+        //compare what we're expecting vs the actual
         assertEquals("Harry", result.get(0).getName());
+        assertEquals("Potions", result.get(0).getSubject());
+    }
+
+    @Test
+    public void gradeIndexTest(){
+        Grade grade = new Grade("Harry", "Potions", "C-");
+        when(gradeRepository.getGrades()).thenReturn(
+        Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        List<Grade> result = gradeService.getGrades();
+        int valid = gradeService.getGradeIndex(result.get(0).getId());
+        int notFound = gradeService.getGradeIndex("123");
+
+        assertEquals(0, valid);
+        assertEquals(Constants.NOT_FOUND, notFound);
+    }
+
+    @Test
+    public void returnGradeByIdTest(){
+        Grade grade = new Grade("Harry", "Potions", "C-");
+        when(gradeRepository.getGrades()).thenReturn(
+        Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        String id = grade.getId();
+        Grade result = gradeService.getGradeById(id);
+        assertEquals(grade, result);
+    }
+
+    @Test
+    public void addGradeTest(){
+        Grade grade = new Grade("Harry", "Potions", "C-");
+        when(gradeRepository.getGrades()).thenReturn(
+        Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        Grade newGrade = new Grade("Hermoine", "Arithmancy", "A+");
+        gradeService.submitGrade(newGrade);
+
+        verify(gradeRepository, times(1)).addGrade(newGrade);
+    }
+
+    @Test
+    public void updateGradeTest(){
+        Grade grade = new Grade("Harry", "Potions", "C-");
+        when(gradeRepository.getGrades()).thenReturn(
+        Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        grade.setScore("F");
+        gradeService.submitGrade(grade);
+        verify(gradeRepository, times(1)).updateGrade(grade, 0);
     }
 }
