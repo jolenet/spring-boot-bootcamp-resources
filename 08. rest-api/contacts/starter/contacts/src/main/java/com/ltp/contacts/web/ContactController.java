c0cacd854331deea1a6f6631a9770543b5c139b6
Jolene <mjolenetay@gmail.com>
2023-09-07 14:09:18 +0800

    162

--- 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -1,9 +1,12 @@
 package com.ltp.contacts.web;
 
+import java.util.List;
+
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +22,12 @@ public class ContactController {
     
     @Autowired
     private ContactService contactService;
+
+    @GetMapping("/contact/all")
+    public ResponseEntity<List<Contact>> getContacts(){
+        List<Contact> contacts = contactService.getContacts();
+        return new ResponseEntity<>(contacts, HttpStatus.OK);
+    }
 
     @GetMapping("/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id){
@@ -36,6 +45,12 @@ public class ContactController {
     public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact){
         contactService.updateContact(id, contact);
         return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/contact/{id}")
+    public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id){
+        contactService.deleteContact(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
