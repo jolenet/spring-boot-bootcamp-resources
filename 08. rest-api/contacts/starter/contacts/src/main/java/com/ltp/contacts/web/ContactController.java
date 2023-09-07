0b89ea7ea36253275fbefc74885eb424e8032430
Jolene <mjolenetay@gmail.com>
2023-09-07 09:13:00 +0800

    157 p2

--- 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -2,7 +2,11 @@ package com.ltp.contacts.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
 @Controller
@@ -10,5 +14,11 @@ public class ContactController {
     
     @Autowired
     private ContactService contactService;
+
+    @GetMapping("/contact/{id}")
+    @ResponseBody   //serialise the data into JSON 
+    public Contact getContact(@PathVariable String id){
+        return new Contact("123", "Jon Snow", "41235122");
+    }
 
 }
